bafbba7fb0df64a4250928651ddb5a2b4f34e0ec
Brandon Cho <brandonbccho@gmail.com>
2025-03-11 22:13:33 -0700

    fixed typos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,10 +22,10 @@ export const Navbar = ({menuOpen, setMenuOpen}) => {
                     </div>
 
                     <div className="hidden md:flex items-center space-x-8">
-                        <a href="#home" className="tex-grey-300 hove:text-white transition-colors">{" "}Home{" "}</a>
-                        <a href="#about" className="tex-grey-300 hove:text-white transition-colors">{" "}About{" "}</a>
-                        <a href="#projects" className="tex-grey-300 hove:text-white transition-colors">{" "}Projects{" "}</a>
-                        <a href="#contact" className="tex-grey-300 hove:text-white transition-colors">{" "}Contact{" "}</a>
+                        <a href="#home" className="text-gray-300 hover:text-white transition-colors">Home</a>
+                        <a href="#about" className="text-gray-300 hover:text-white transition-colors">About</a>
+                        <a href="#projects" className="text-gray-300 hover:text-white transition-colors">Projects</a>
+                        <a href="#contact" className="text-gray-300 hover:text-white transition-colors">Contact</a>
                     </div>
                 </div>
             </div>            
