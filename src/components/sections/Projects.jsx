fde0017184554bfd689baa8bf2a73dca2ef4fc95
Brandon Cho <brandonbccho@gmail.com>
2025-06-03 14:43:46 -0700

    added wishlist project

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -37,15 +37,14 @@ export const Projects = () => {
                             </div>
                         </div>
                         
-                        {/* uncomment after adding more projects
                         <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
-                            <h3 className="text-xl font-bold mb-2">Cloud Platform</h3>
+                            <h3 className="text-xl font-bold mb-2">Your Wishlist</h3>
                             <p className="text-grey-400 mb-4">
-                                A cloud platform that allows users to store and share files.
+                                Web application that allows users to add, edit, and delete items to their wishlist.
                             </p>
 
                             <div className="flex flex-wrap gap-2 mb-4">
-                                {["React", "Node.js", "Docker"].map((tech, key) => (
+                                {["React", "Node.js", "Express", "PostgreSQL"].map((tech, key) => (
                                     <span 
                                     key={key}
                                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition">
@@ -55,12 +54,13 @@ export const Projects = () => {
                             </div>
 
                             <div className="flex justify-between items-center">
-                                <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors my-4">
+                                <a href="https://github.com/brndncho/your-wishlist" className="text-blue-400 hover:text-blue-300 transition-colors my-4">
                                     View Project ➜
                                 </a>
                             </div>
                         </div>
-
+                        
+                        {/* uncomment after adding more projects
                         <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
                             <h3 className="text-xl font-bold mb-2">Cloud Platform</h3>
                             <p className="text-grey-400 mb-4">
